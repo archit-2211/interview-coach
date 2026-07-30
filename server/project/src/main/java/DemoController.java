a20c525113445ca8ddf52087bedba9e4c513c5ae
Archit Kumar Agarwal <archit2211@Archits-MacBook-Pro.local>
2026-07-30 14:21:46 +0530

    DemoController added

---
+++ server/project/src/main/java/DemoController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Interview Coach Backend is Live!";
+    }
+}
