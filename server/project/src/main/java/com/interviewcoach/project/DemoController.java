3f84654de326ba1ca0c722c2834d40271a71f4e2
Archit Kumar Agarwal <archit2211@Archits-MacBook-Pro.local>
2026-07-30 14:23:09 +0530

    Demo Controller change

--- server/project/src/main/java/com/interviewcoach/project/DemoController.java
+++ server/project/src/main/java/com/interviewcoach/project/DemoController.java
@@ -1,3 +1,4 @@
+package com.interviewcoach.project;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
